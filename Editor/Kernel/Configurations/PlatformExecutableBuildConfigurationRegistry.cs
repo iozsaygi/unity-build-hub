aab3f3e670a34763bf57d4509d0fc8539ba78b8f
İsmail Özsaygı <devozsaygi@gmail.com>
2023-12-25 22:24:38 +0300

    Added Windows x64 Mono configuration to registry

--- Editor/Kernel/Configurations/PlatformExecutableBuildConfigurationRegistry.cs
+++ Editor/Kernel/Configurations/PlatformExecutableBuildConfigurationRegistry.cs
@@ -10,7 +10,28 @@ namespace UnityBuildHub.Editor.Kernel.Configurations
 {
     internal static class PlatformExecutableBuildConfigurationRegistry
     {
-        // ReSharper disable once IdentifierTypo
+        // Windows x64 Mono configuration.
+        internal static readonly PlatformExecutableBuildConfiguration
+            WindowsX64MonoPlatformExecutableBuildConfiguration = new
+            (
+                new BuildPlayerOptions()
+                {
+                    scenes = BuildOperations.FindAvailableScenesForBuild(),
+                    locationPathName = BuildOperations.FindTargetBuildFilePath(BuildTarget.StandaloneWindows64),
+                    targetGroup = BuildTargetGroup.Standalone,
+                    target = BuildTarget.StandaloneWindows64,
+                    options = BuildOptions.StrictMode | BuildOptions.DetailedBuildReport
+                },
+                ScriptingImplementation.Mono2x,
+                new IPreBuildTask[]
+                {
+                },
+                new IPostBuildTask[]
+                {
+                }
+            );
+
+        // MacOS x64 IL2CPP configuration.
         internal static readonly PlatformExecutableBuildConfiguration
             MacOSX64IL2CPPPlatformExecutableBuildConfiguration = new
             (
