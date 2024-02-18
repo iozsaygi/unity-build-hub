f743a345aa17e1b70ad23d0bf866ab34a87a4ddf
İsmail Özsaygı <devozsaygi@gmail.com>
2024-02-18 21:31:43 +0300

    Updated menu item provider with new builder endpoints

--- Editor/Shared/MenuItemProvider.cs
+++ Editor/Shared/MenuItemProvider.cs
@@ -8,6 +8,18 @@ namespace UnityBuildHub.Editor.Shared
     /// </summary>
     internal static class MenuItemProvider
     {
+        [MenuItem("Unity Build Hub/Build/Windows x64 Mono")]
+        internal static void ExecuteWindowsX64MonoBuild()
+        {
+            CIEndpointProvider.WindowsX64Mono();
+        }
+
+        [MenuItem("Unity Build Hub/Build/Windows x64 IL2CPP")]
+        internal static void ExecuteWindowsX64IL2CPPBuild()
+        {
+            CIEndpointProvider.WindowsX64IL2CPP();
+        }
+
         [MenuItem("Unity Build Hub/Build/MacOS x64 IL2CPP")]
         internal static void ExecuteMacOSX64IL2CPPBuild()
         {
