7db67c1750b9a4845480325656ce5475b740a710
İsmail Özsaygı <devozsaygi@gmail.com>
2024-08-25 20:25:43 +0300

    Added commands for build config. properties

--- Editor/Kernel/Configurations/PlatformExecutableBuildConfiguration.cs
+++ Editor/Kernel/Configurations/PlatformExecutableBuildConfiguration.cs
@@ -6,9 +6,16 @@ namespace UnityBuildHub.Editor.Kernel.Configurations
 {
     public readonly struct PlatformExecutableBuildConfiguration
     {
+        // Native options for Unity's build pipeline.
         public readonly BuildPlayerOptions BuildPlayerOptions;
+
+        // The type of scripting backend for the build. (Mono, IL2CPP etc.)
         public readonly ScriptingImplementation ScriptingImplementation;
+
+        // Set of tasks that are going to be executed before actual Unity build.
         public readonly IPreBuildTask[] PreBuildTasks;
+
+        // Set of tasks that are going to be executed after actual Unity build completes.
         public readonly IPostBuildTask[] PostBuildTasks;
 
         public PlatformExecutableBuildConfiguration(BuildPlayerOptions buildPlayerOptions,
